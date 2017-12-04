afeed93d002dff7aa882789e963078507d6c3d7d
mminsk <mcm1900@outlook.com>
2017-12-04 12:21:13 +0100

    ui for home page

--- app/src/main/java/hu/ait/android/aitlastchances/MainActivity.java
+++ app/src/main/java/hu/ait/android/aitlastchances/MainActivity.java
@@ -1,10 +1,13 @@
 package hu.ait.android.aitlastchances;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         return true;
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_matches:
+                    return true;
+                case R.id.navigation_connections:
+                    return true;
+            }
+            return false;
+        }
+
+    };
 }
