10f5048ed935a608dc82e64dba344ba3baf30174
mminsk <mcm1900@outlook.com>
2017-12-13 23:51:30 +0100

    UI updates

--- app/src/main/java/hu/ait/android/aitlastchances/MatchesActivity.java
+++ app/src/main/java/hu/ait/android/aitlastchances/MatchesActivity.java
@@ -1,29 +1,17 @@
 package hu.ait.android.aitlastchances;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.firebase.auth.FirebaseAuth;
-
-import java.util.ArrayList;
 
 import hu.ait.android.aitlastchances.data.ConnectionMatch;
 
@@ -36,7 +24,7 @@ public class MatchesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connections);
+        setContentView(R.layout.activity_matches);
 
         myUsername = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
 
